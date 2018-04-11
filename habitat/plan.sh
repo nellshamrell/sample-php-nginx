996ae4cd1e0559feec744309e6244c02fdeb5d5c
Nell Shamrell <nellshamrell@gmail.com>
2018-04-11 16:11:10 -0700

    appears to work
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -4,32 +4,11 @@ pkg_version="0.1.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_source="https://github.com/nellshamrell/php-test/archive/0.1.0.tar.gz"
-# pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
 pkg_shasum="bde86f0d969211d3ad9162024785a414805a2247296821c9a1c9a2d37c03c375"
 pkg_deps=(core/php/7.1.4/20180411221100)
-# pkg_build_deps=(core/make core/gcc)
-# pkg_lib_dirs=(lib)
-# pkg_include_dirs=(include)
-# pkg_bin_dirs=(bin)
-# pkg_pconfig_dirs=(lib/pconfig)
-# pkg_svc_run="haproxy -f $pkg_svc_config_path/haproxy.conf"
-# pkg_exports=(
-#   [host]=srv.address
-#   [port]=srv.port
-#   [ssl-port]=srv.ssl.port
-# )
-# pkg_exposes=(port ssl-port)
-# pkg_binds=(
-#   [database]="port host"
-# )
-# pkg_binds_optional=(
-#   [storage]="port host"
-# )
-# pkg_interpreters=(bin/bash)
-# pkg_svc_user="hab"
-# pkg_svc_group="$pkg_svc_user"
-# pkg_description="Some description."
-# pkg_upstream_url="http://example.com/project-name"
+
+pkg_svc_user=root
+pkg_svc_group=$pkg_svc_user
 
 do_build() {
     return 0
